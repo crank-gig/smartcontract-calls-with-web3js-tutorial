26a3e7e88ce0bd5f65b8350fa205971929f33280
crank-gig <crankgig@gmail.com>
2023-11-10 04:38:15 +0100

    Updating earlier fabricated credentials with real value

--- scripts/interact.js
+++ scripts/interact.js
@@ -3,13 +3,10 @@ const Contract = require('web3-eth-contract');
 // set provider for all later instances to use
 Contract.setProvider(window.ethereum);
 
-// Fabricated address for the "GoFundMia" smart contract
-const contractAddress = "0x1234567890abcdef1234567890abcdef1234567890";
-// Fabricated ABI for the "GoFundMia" smart contract (simplified for this example)
-const contractABI = [
-    { constant: true, inputs: [], name: "checkDonation", outputs: [{ name: "", type: "uint256" }], payable: false, stateMutability: "view", type: "function" },
-    { constant: false, inputs: [], name: "donate", outputs: [], payable: true, stateMutability: "payable", type: "function" }
-  ];
+//"GoFundMia" smart contract address
+const contractAddress = "0xB57c7EF9BE454b5200704F8Eb8C112920Db7697D";
+//GoFundMia" smart contract ABI
+const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"DonationReceived","type":"event"},{"inputs":[],"name":"donate","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalDonation","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdrawFunds","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 
 
 //instantiate the smart contract as follows:
@@ -19,12 +16,12 @@ const loadDonatedAmount = async () => {
     // Define an asynchronous function called 'loadCurrentMessage'.
     
     const amount = await goFundMiaContract.methods.totalDonation().call();
-    // Using the 'helloWorldContract' instance, this line calls a function named 'totalDonation' on the smart contract.
-    // The 'call()' function is used to read data from the smart contract without creating a transaction.
-    // The result of this call is in Wei and is stored in the 'amount' variable.
+    // Using the helloWorldContract instance, this line calls a function named totalDonation on the smart contract.
+    // The call() function is used to read data from the smart contract without creating a transaction.
+    // The result of this call is in Wei and is stored in the amount variable.
   
     return amount;
-    // The function returns the value obtained from calling 'totalDonation' on the smart contract.
+    // The function returns the value obtained from calling totalDonation on the smart contract.
 };
 
 const donate = async (address,amount) => {
